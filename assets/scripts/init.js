5e655ee78912403d5e6d99ea13286443853e69f8
Alexander Cleary <alexjacleary@gmail.com>
2024-03-12 13:08:51 +1100

    Thumbnails + Buttons

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -7,7 +7,7 @@ const experienceData =  {
             end: 2023,
             pinned: true,
             description: "One-on-one tutoring students in various computer and programming skills.",
-            image: "",
+            image: "tutoring.jpg",
             button: "",
             id: 0,
         },
@@ -18,7 +18,7 @@ const experienceData =  {
             pinned: true,
             end: 2023,
             description: "Working on a research project, conversing with PhD and post-doctorate researchers, attending astrophysics meetings, touring the Australian Synchrotron, and learning about science communication.",
-            image: "",
+            image: "physics_and_astronomy.png",
             button: "",
             id: 1,
         },
@@ -28,7 +28,7 @@ const experienceData =  {
             start: 2022,
             end: 2023,
             description: "Teaching a small class of students aged ten to fourteen how to code using Scratch over a week-long course, including one-on-one teaching and presenting content to a group, planning, building, marketing and facilitating a business.",
-            image: "",
+            image: "code_club.jpg",
             button: "",
             id: 2,
             pinned: true,
@@ -41,7 +41,7 @@ const experienceData =  {
             start: 2022,
             end: 2023,
             description: "Working in a Kitchen-Hand role which involved preparing and serving meals, washing dishes, delivering meals to rooms and interacting with the residents..",
-            image: "",
+            image: "westmont.jpg",
             button: "",
             id: 0,
         },
@@ -51,7 +51,7 @@ const experienceData =  {
             start: 2022,
             end: null,
             description: "Cast member in production produced by Scouts, involving participating in rehearsals and live performances at the Albury Entertainment Centre, and collaborating on script writing at creative meetings.",
-            image: "",
+            image: "gang_show.jpg",
             button: "",
             id: 1,
         },
@@ -61,7 +61,7 @@ const experienceData =  {
             start: 2015,
             end: 2023,
             description: "Member of the scouts, participating in extreme activities, gaining skills in teamwork, adaptability and leadership. Assisted in many community projects and fundraising activities, including selling chocolates, sausage sizzles and raffles.",
-            image: "",
+            image: "scouts.jpg",
             button: "",
             id: 2,
         },
@@ -71,7 +71,7 @@ const experienceData =  {
             start: 2020,
             end: 2021,
             description: "Cadet in the Australian Air Force Cadets program, gaining leadership, navigation, survival, decision-making, bush-craft and obedience skills.",
-            image: "",
+            image: "air_force_cadets.png",
             button: "",
             id: 3,
         },
@@ -83,8 +83,9 @@ const experienceData =  {
             start: 2023,
             end: 2024,
             description: "Building a new website for the Albury Gang Show, a volunteer-led theatre company, acquiring skills such as front-end JavaScript, parallax and loading screens.",
-            image: "",
-            button: "",
+            image: "albury_gang_show.png",
+            button: "https://alburygangshow.com.au",
+            buttonText: "Visit",
             id: 0,
             pinned: true,
         },
@@ -94,9 +95,9 @@ const experienceData =  {
             start: 2023,
             end: 2024,
             description: "Building a personal website for showcasing my experience and storing personal projects, gaining skills such as front-end web development in HTML/CSS, alongside learning systems including git.",
-            image: "",
-            button: "",
-            id: 1,
+            image: "cleary_coding.png",
+            button: "https://clearycoding.com",
+            buttonText: "Visit",
             pinned: true,
         },
     ]
@@ -104,7 +105,7 @@ const experienceData =  {
 const journalData = [
     {
         title: "Journal Page Test",
-        id: 0,
+        image: "",
         pinned: true,
         description: "Hi",
         date: "NaN",
@@ -149,14 +150,14 @@ document.addEventListener('DOMContentLoaded', () => {
             if (data[j].pinned === true) {
                 carousel.innerHTML += `
                 <article id="${section}-${data[j].id}" class="carousel-item">
-                    <img src="/assets/images/${section}-${i === 1 ? 'stem-' : ''}thumbnails/item-${data[j].id}.png" alt="Thumbnail" class="carousel-item-thumbnail">
+                    <img src="${data[j].image ?`/assets/images/${["portfolio", "experience", "portfolio"][i]}-thumbnails/${data[j].image}` : '/assets/images/thumbnail.png'}" alt="Thumbnail" class="carousel-item-thumbnail">
                     <section class="carousel-item-text">
-                        <time class="carousel-item-text-date">${data[j].date ? `${data[j].date}` : `${data[j].end ? `${data[j].start} - ${data[j].end}`: `${data[j].start}`}`}</time>
+                        <time class="carousel-item-text-date">${data[j].date ? `${data[j].date}` : `${data[j].end ? `${data[j].start === data[j].end ? `${data[j].start}` : `${data[j].start} - ${data[j].end}`}`: `${data[j].start} - Current`}`}</time>
                         <h1 class="carousel-item-text-title">${data[j].title}</h1>
                         <p class="carousel-item-text-description">
                             ${data[j].description}
                         </p>
-                        ${data[j].button ? `<a href=${data[j].button}><div class="button"> <div class="button-text">More &#8594</div> </div></a>` : ''}
+                        ${data[j].button ? `<a href=${data[j].button}><div class="button"> <div class="button-text">${data[j].buttonText ? `${data[j].buttonText}` : 'More'} &#8594</div> </div></a>` : ''}
                     </section>
                 </article>
                 `
